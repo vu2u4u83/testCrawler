5d6e19e1b1d2e188c80bbd0553684d494de062f1
YILE.NET\jackwu <jackwu@yile.com.tw>
2020-09-22 11:28:45 +0800

    解析陣列並轉為List清單

--- src/main/java/model/Crawler.java
+++ src/main/java/model/Crawler.java
@@ -2,10 +2,14 @@ package model;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import util.HttpClientUtil;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Crawler {
 
@@ -18,7 +22,17 @@ public class Crawler {
         try {
             Document doc = Jsoup.parse(httpClientUtil.get(url, httpClientUtil.DATA_JSON_HEADERS).getHttpResponse());
 
-            System.out.println(doc);
+            Elements tr = doc.select("tr");
+
+            List<String> txt = new ArrayList();
+
+            for (Element element : tr){
+                txt.add(element.text());
+            }
+
+            for (String aa : txt){
+                System.out.println(aa);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
