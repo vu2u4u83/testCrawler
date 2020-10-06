9edfb32ed3d1a31e1fd0baf66b179d0b3eca2829
YILE.NET\jackwu <jackwu@yile.com.tw>
2020-10-06 16:39:57 +0800

    更換網址並解析出Title & 增加時間計算

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Crawler crawler = new Crawler();
-        crawler.getPageResponse("http://bf9a285c03b490c9.api.sstream365.net/?lng=en");
+        crawler.getPageResponse();
 
     }
 
--- src/main/java/model/Crawler.java
+++ src/main/java/model/Crawler.java
@@ -1,45 +1,65 @@
 package model;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import util.HttpClientUtil;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Crawler {
 
-    HttpClientUtil httpClientUtil = new HttpClientUtil();
+    public void getPageResponse(){
 
-    public void getPageResponse(String url){
-
-        httpClientUtil.create();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String content = null;
 
         try {
-            Document doc = Jsoup.parse(httpClientUtil.get(url, httpClientUtil.DATA_JSON_HEADERS).getHttpResponse());
 
-            Elements tr = doc.select("tr");
+            //網址
+            String url = "https://www.imdb.com/chart/top/";
 
-            List<String> txt = new ArrayList();
+            long startTime = new Date().getTime();
 
-            for (Element element : tr){
-                txt.add(element.text());
-            }
+            //使用 Http 拿取資料
+            HttpGet get = new HttpGet(url);
 
-            for (String aa : txt){
-                System.out.println(aa);
-            }
+            response = httpclient.execute(get);
+
+            long getTime = new Date().getTime();
+            System.out.println("得到回應 : " + this.processDate(new Date(getTime - startTime)));
+
+            HttpEntity entity = response.getEntity();
+
+            content = EntityUtils.toString(entity);
+
+            //使用 jsoup 做頁面的處理
+            Document doc = Jsoup.parse(content);
+            Elements td = doc.select("tr > td > a[href]");
+            System.out.println(td.text());
+
+            long endTime = new Date().getTime();
+            System.out.println("處理結束 : " + this.processDate(new Date(endTime - startTime)));
 
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
         }
 
+    }
+
+    public String processDate(Date date){
+
+        SimpleDateFormat format = new SimpleDateFormat("ss.SSS");
+
+        return format.format(date);
     }
 
 }
