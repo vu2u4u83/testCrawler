cc91637d1917bdb5250310a9784dc8fb4b947178
vu2u4u83 <vu2u4u83@gmail.com>
2021-05-04 01:54:27 +0800

    20210504 修正可爬https

--- src/main/java/model/Crawler.java
+++ src/main/java/model/Crawler.java
@@ -1,16 +1,54 @@
 package model;
 
-import org.apache.http.HttpEntity;
+import org.apache.http.*;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.AuthSchemes;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.*;
+import org.apache.http.conn.DnsResolver;
+import org.apache.http.conn.HttpConnectionFactory;
+import org.apache.http.conn.ManagedHttpClientConnection;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.DefaultHttpResponseFactory;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.*;
+import org.apache.http.impl.io.DefaultHttpRequestWriterFactory;
+import org.apache.http.io.HttpMessageParser;
+import org.apache.http.io.HttpMessageParserFactory;
+import org.apache.http.io.HttpMessageWriterFactory;
+import org.apache.http.io.SessionInputBuffer;
+import org.apache.http.message.BasicLineParser;
+import org.apache.http.message.LineParser;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.CharArrayBuffer;
 import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.CodingErrorAction;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -18,14 +56,40 @@ public class Crawler {
 
     public void getPageResponse(){
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
         CloseableHttpResponse response = null;
         String content = null;
 
+        X509TrustManager tm = new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+
         try {
 
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[] { tm }, null);
+
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+            CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf)
+                                                                    .setMaxConnTotal(50)
+                                                                    .setMaxConnPerRoute(50)
+                                                                    .setDefaultRequestConfig(RequestConfig.custom()
+                                                                    .setConnectionRequestTimeout(60000)
+                                                                    .setConnectTimeout(60000)
+                                                                    .setSocketTimeout(60000).build()).build();
+
             //網址
-            String url = "https://kma.kkbox.com/charts/yearly/newrelease?lang=tc&terr=tw";
+            String url = "https://www.ettoday.net/news/news-list.htm";
 
             long startTime = new Date().getTime();
 
@@ -42,7 +106,7 @@ public class Crawler {
 
             //使用 jsoup 做頁面的處理
             Document doc = Jsoup.parse(content);
-            Elements tr = doc.select(".charts-list-row");
+            Elements tr = doc.select(".part_list_2");
 
             long endTime = new Date().getTime();
 
@@ -52,6 +116,10 @@ public class Crawler {
                                 + this.parserDate(new Date(startTime)));
 
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
             e.printStackTrace();
         }
 
@@ -70,5 +138,7 @@ public class Crawler {
 
         return format.format(date);
     }
+
+
 
 }
